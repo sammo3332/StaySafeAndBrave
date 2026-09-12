d1c2f7be0c482595eab17f5a5ed7b6d4da0dfeff
Houssam <houssamtaleb009@gmail.com>
2026-09-12 14:46:32 +0200

    fix(firebase): point web app to production project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "brave-guides",
-  "appId": "1:206153793979:web:75535715d987bf71c2449e",
-  "apiKey": "AIzaSyCjqKhVW19ykrNt38CDmYnvcrO_dvnAR74",
-  "authDomain": "brave-guides.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "206153793979"
+  projectId: "gen-lang-client-0879511720",
+  appId: "1:1065247497142:web:b8e589d94397861bbc3f17",
+  apiKey: "AIzaSyBYFJ7zE_P08vKbwuTF23Ll_3Bn_-zS3eY",
+  authDomain: "gen-lang-client-0879511720.firebaseapp.com",
+  measurementId: "G-J6F6SQV2V7",
+  messagingSenderId: "1065247497142",
+  storageBucket: "gen-lang-client-0879511720.firebasestorage.app"
 };
